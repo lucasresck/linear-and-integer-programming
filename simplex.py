7f07d3fbebec9515626bac5d1f2a409e0d98ab90
Lucas Emanuel Resck Domingues <41991486+lucasresck@users.noreply.github.com>
2021-05-01 21:21:57 -0300

    Start simplex implementation

---
+++ simplex.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+class Simplex:
+    def __init__(self, tableau):
+        self.tableau = tableau
+        self._simplex()
+
+    def simplex(self):
+        '''Apply simplex method to the tableau.'''
+        pass

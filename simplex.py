6a2d73f9ecf630b3a20b067c91f352f201b12ce7
Lucas Emanuel Resck Domingues <41991486+lucasresck@users.noreply.github.com>
2021-05-01 23:53:12 -0300

    Solve simplex in canonical form

--- simplex.py
+++ simplex.py
@@ -1,10 +1,100 @@
 import numpy as np
 
 class Simplex:
-    def __init__(self, tableau):
-        self.tableau = tableau
-        self._simplex()
+    def __init__(self, tableau, max_iterations=10000):
+        self.tableau = tableau.astype('float')
+        self.max_iterations = max_iterations
 
     def simplex(self):
-        '''Apply simplex method to the tableau.'''
-        pass
+        '''
+        Apply simplex method to a tableau in canonical form.
+
+        Maximize    z = c1 x1 + ... + c4 x4 + z0
+        subject to
+                    a11 x1 + ... + a14 x4 = b1
+                    a21 x1 + ... + a24 x4 = b2
+                    xi >= 0
+
+        We also have
+        -- bi >= 0
+        -- each constraint has a decision variable xi with coefficient +1
+        -- isolated variables do not appear in other constraints
+        -- isolated variables have coefficient zero in objective function
+
+        The representation is a tableau such as:
+
+            x1  x2  x3  x4
+        [[  a11 a12 a13 a14 b1]
+        [   a21 a22 a23 a24 b2]
+        [   c1  c2  c3  c4  -z0]]        
+        '''
+
+        m, n = np.array(self.tableau.shape) - 1
+        
+        # Step (0)
+        # The problem is already canonical
+
+        for _ in range(self.max_iterations):
+
+            # Step (1)
+            # We check optimality
+
+            # If we are optimal
+            if self.is_optimal():
+                self.generate_solution(n)
+                break
+            else:
+
+                # Step (2)
+                # Choose column s to pivot
+
+                s = np.argmax(self.tableau[-1])
+                if self.is_unbounded(s):
+                    print('The primal problem is unbounded.')
+                    break
+                else:
+                    
+                    # Step (3)
+                    # Choose row r to pivot
+
+                    r = self.ratio_test(m, s)
+
+                    # Normalization
+                    self.tableau[r] = self.tableau[r] / self.tableau[r, s]
+
+                    # Pivoting
+                    self.pivoting(m, r, s)
+
+    def is_optimal(self):
+        '''Check optimality for canonical form.'''
+        return not np.sum(self.tableau[-1, :-1] > 0)
+
+    def generate_solution(self, n):
+        '''Generate solution for the optimal canonical form.'''
+        self.solution = []
+        for j in range(n):
+            if self.tableau[-1][j] != 0:
+                self.solution.append(0)
+            else:
+                i = np.where(self.tableau[:, j] == 1)[0][0]
+                self.solution.append(self.tableau[i, -1])
+        self.solution = np.array(self.solution)
+        self.objective = np.dot(self.solution, self.tableau[-1, :-1]) - self.tableau[-1, -1]
+
+    def is_unbounded(self, s):
+        '''Check is objective function is unbounded.'''
+        return not np.sum(self.tableau[:-1, s] > 0)
+
+    def ratio_test(self, m, s):
+        '''Ratio test for choosing which variable will be introduced into the basis.'''
+        b = self.tableau[:-1, -1]
+        a_s = self.tableau[:-1, s]
+        r = np.argmin([b[i]/a_s[i] if a_s[i] > 0 else np.inf for i in range(m)])
+        return r
+
+    def pivoting(self, m, r, s):
+        '''Replace the basic variable in row r with variable s via pivoting.'''
+        for i in range(m + 1):
+            if i == r:
+                continue
+            self.tableau[i] = self.tableau[i] - self.tableau[r] * self.tableau[i, s]
